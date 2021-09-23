13933256ba7c12272b3666c028a1c2562570d463
phananhtai1 <1751010128tai@ou.edu.vn>
2021-09-23 10:30:26 +0700

    Food Ápp

---
+++ FoodOrderApp/app/src/main/java/com/android/foodorderapp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.android.foodorderapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        TextView buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
